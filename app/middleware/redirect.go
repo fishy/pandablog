7e5b5bf4fcc5c14ef49f1348523c755f0ae846f3
Lee Cheng Hui <chlee9926@gmail.com>
2023-03-19 13:15:41 +0800

    Check if app is behind reverse proxy

--- app/middleware/redirect.go
+++ app/middleware/redirect.go
@@ -11,10 +11,35 @@ import (
 // Redirect will handle all redirects required.
 func (c *Handler) Redirect(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Currently the following code will cause trouble when this application is
+		// running behind a reverse proxy and have a domain name. The reverse proxy
+		// will set the Host header to the domain name, and the code below will
+		// redirect to the domain name. This will cause the reverse proxy to
+		// redirect to itself, and the redirect will be infinite.
+		// To fix it, we need to add a configuration to tell the application that
+		// it is running behind a reverse proxy, and the reverse proxy will set
+		// the Host header to the domain name. Then we can skip the redirect
+		// code below.
 		// Redirect to the correct website.
-		if !envdetect.RunningLocalDev() && len(c.SiteURL) > 0 && !strings.Contains(r.Host, c.SiteURL) {
-			http.Redirect(w, r, fmt.Sprintf("%v://%v%v", c.SiteScheme, c.SiteURL, r.URL.Path), http.StatusPermanentRedirect)
-			return
+
+
+		// if !envdetect.RunningLocalDev() && len(c.SiteURL) > 0 && !strings.Contains(r.Host, c.SiteURL){
+		// 	http.Redirect(w, r, fmt.Sprintf("%v://%v%v", c.SiteScheme, c.SiteURL, r.URL.Path), http.StatusPermanentRedirect)
+		// 	return
+		// }
+		
+		if !envdetect.RunningLocalDev() && len(c.SiteURL) > 0 {
+			var host string
+			if len(r.Header.Get("X-Forwarded-Host")) > 0 {
+			  host = r.Header.Get("X-Forwarded-Host")
+			} else {
+			  host = r.Host
+			}
+			if !strings.Contains(host, c.SiteURL) {
+			  http.Redirect(w, r, fmt.Sprintf("%v://%v%v", c.SiteScheme, c.SiteURL, r.URL.Path), http.StatusPermanentRedirect)
+			  return
+			}
 		}
 
 		// Don't allow access to files with a slash at the end.
